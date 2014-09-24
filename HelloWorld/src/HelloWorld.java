c2f83bec8e85799e1ad1f78573cc106219427f12
Yuki Sasaki <yusasaki4+git@gmail.com>
2014-09-24 22:05:17 +0900

    first commit for test
    Signed-off-by: Yuki Sasaki <yusasaki4+git@gmail.com>

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,42 @@
+/**
+ * File: HelloWorld.java
+ * Package: 
+ * Project: HelloWorld
+ * 
+ * Creation Date: 2014/09/24
+ * Author: Yuki
+ * 
+ * Note: 
+ * 
+ * 
+ * Last commit: $Date$
+ * Revision: $Rev$
+ * Commit User: $Author$
+ * 
+ * Copyright (c) 2014, Yuki All rights reserved.
+ */
+
+/**
+ * 
+ * 
+ * @author Yuki
+ *
+ */
+public class HelloWorld {
+    
+    /**
+     * 
+     */
+    public HelloWorld() {
+        // do nothing
+    }
+    
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+        
+    }
+    
+}
